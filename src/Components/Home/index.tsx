750136d3fc3bd868263b19616c26eac8191ee688
jahanara khatun <zarakhatun58@gmail.com>
2022-11-15 19:14:41 +0530

    update

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import SearchBar from '../SearchBar';
 
 const Home = () => {
-    //Get Current post 
-//   const indexOfLastPost=currentPage* postsPerPage;
-//   const indexOfLastPost=indexOfLastPost-  postsPerPage;
-//   const currentPosts=posts.slice(indexOfFirstPost, indexOfLastPost);
+
     return (
         <div>
             <SearchBar />
